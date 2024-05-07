5313056f832f5c170e718fae48e5d0f35f9cabe9
Home <Home@LAPTOP-2E6V6ACM>
2024-05-07 12:38:01 +0300

    Update PPS

--- LoginDemoServer/DBScripts/Create.sql
+++ LoginDemoServer/DBScripts/Create.sql
@@ -12,7 +12,7 @@ Create Table Users (
 )
 Go
 
-INSERT INTO dbo.Users VALUES ('talsi@talsi.com', '1234', '+972503401511','20-may-1976',1,'Tal Simon')
+INSERT INTO dbo.Users VALUES ('talsi@talsi.com', '1234', '+972506665835','20-may-1976',1,'Tal Simon')
 Go
 
 --scaffold-DbContext "Server = (localdb)\MSSQLLocalDB;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
